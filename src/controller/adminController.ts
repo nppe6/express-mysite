3877db0c8c26db8027a0637c57a7f4240bfaa2cd
nppe6 <xiao2313667401@foxmail.com>
2025-01-26 16:35:16 +0800

    验证token 是否有效中间件完成 以及恢复登录状态接口已完成

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -3,13 +3,22 @@ import { AdminInput } from '../middleware/validator/admin.validator'
 import commonRes from '../utils/commonRes'
 import silentHandle from '../utils/silentHandle'
 import adminService from '../service/adminService'
+import { CustomRequest, UserInfoInterface } from '../middleware/jwt'
 
+// 登录controller
 const login = async (req: Request<{}, {}, AdminInput>, res: Response) => {
   const [e, user] = await silentHandle(adminService.login, req.body)
 
   return e ? commonRes.error(res, null, e.message) : commonRes(res, user, { message: '登录成功' })
 }
 
+// 恢复登录 controller
+const whoami = async (req: CustomRequest<UserInfoInterface>, res: Response) => {
+  const { exp, iat, loginPwd, ...data } = req.userInfo as UserInfoInterface
+  return commonRes(res, data)
+}
+
 export default {
   login,
+  whoami,
 }
--- src/router/admin.ts
+++ src/router/admin.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import adminController from '../controller/adminController'
 import adminValidator from '../middleware/validator/admin.validator'
+import { verifyToken } from '../middleware/jwt'
 
 const adminRouter = Router()
 
-adminRouter.post('/login', adminValidator.login, adminController.login)
+adminRouter
+  .post('/login', adminValidator.login, adminController.login)
+  .get('/whoami', verifyToken(), adminController.whoami)
 
 export default adminRouter
